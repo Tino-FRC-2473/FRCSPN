6ccc04d5d5ba1cef3ccc0a31bab34f3e24fda7fa
Joseph Michels <bobelement4181@gmail.com>
2018-02-18 18:55:06 -0800

    some work on python

--- python/get3DArray.py
+++ python/get3DArray.py
@@ -1,14 +1,83 @@
 import os
 import pprint
 import json
+import requests
 
-files = []
+allMatches = []
 
-for folder in os.listdir("docs"):
-	for file in os.listdir("docs/"+folder):
+for folder in os.listdir("docs/matches"):
+	for file in os.listdir("docs/matches/"+folder):
 		if file.endswith(".json"):
-			files.append(os.path.join("docs/"+folder, file))
+			allMatches.append(json.load(open(os.path.join("docs/matches/"+folder, file))))
 
-for file in files:
-	match = json.load(open(file))
-	pprint.pprint(match)
+teams = []
+with open("docs/teams.txt", "r") as f:
+	for line in f:
+	    teams.append(line[:len(line)-1])
+print(teams)
+
+data = []
+
+def getValue(match, key):
+	for thing in match:
+		if thing["name"] == key:
+			return thing["size"]
+
+def getTeams(match):
+	arr = []
+	for i in range(3):
+		arr.append(getValue(match, "alliances")['red']['team_keys'][i])
+		arr.append(getValue(match, "alliances")['blue']['team_keys'][i])
+	return arr
+
+for team in teams:
+	tArr = []
+	for match in allMatches:
+		mArr = []
+		if(team in getTeams(match)):
+			#mArr.append(getValue(match, "key"))
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["totalPoints"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["totalPoints"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["autoSwitchOwnershipSec"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["autoSwitchOwnershipSec"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["autoScaleOwnershipSec"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["autoScaleOwnershipSec"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["autoRunPoints"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["autoRunPoints"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["teleopSwitchOwnershipSec"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["teleopSwitchOwnershipSec"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["teleopScaleOwnershipSec"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["teleopScaleOwnershipSec"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultBoostTotal"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultBoostTotal"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultBoostPlayed"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultBoostPlayed"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultForceTotal"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultForceTotal"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultForcePlayed"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultForcePlayed"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultLevitateTotal"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultLevitateTotal"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultLevitatePlayed"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultLevitatePlayed"])
+
+			mArr.append(getValue(match, "score_breakdown")["blue"]["vaultLevitateTotal"])
+			mArr.append(getValue(match, "score_breakdown")["red"]["vaultLevitateTotal"])
+			
+		if len(mArr) > 0:
+			tArr.append(mArr)
+	data.append(tArr)
+
+pprint.pprint(data)
