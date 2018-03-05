5b8070cae695aca2bbb0ea518cccfbb8e4b1ae30
Joseph Michels <bobelement4181@gmail.com>
2018-03-05 13:58:42 -0800

    debugged red/blue with print statement

--- python/constructArray.py
+++ python/constructArray.py
@@ -1,5 +1,5 @@
 import os
-#import pprint
+import pprint
 import json
 import numpy as np
 
@@ -67,14 +67,75 @@ print("\nSelected Events:")
 print("   ", selected)
 print("")
 
-allMatches = []
+matches = []
 i = 0
 
 for key in selected:
 	fullPath = os.getcwd()
 	path = fullPath[:fullPath.rfind("\\")] + "\\data\\" + key + "\\matches"
 	for fName in os.listdir(path):
-		allMatches.append(json.load(open(path + "\\" + fName, "r+")))
+		matches.append(json.load(open(path + "\\" + fName, "r+")))
 
-arr = np.array(allMatches)
-print(arr.shape)
+def getValue(match, key):
+	for thing in match:
+		if thing["name"] == key:
+			return thing["size"]
+
+def getTeams(match):
+	arr = []
+	for i in range(3):
+		arr.append(getValue(match, "alliances")['blue']['team_keys'][i])
+		arr.append(getValue(match, "alliances")['red']['team_keys'][i])
+	return arr
+
+fullTeamData = {}
+teamData = {}
+
+for match in matches:
+	for i, team in enumerate(getTeams(match)):
+		if not team in fullTeamData:
+			fullTeamData[team] = {"switch": [], "scale": [], "oppSwitch": [], "auto": [], "endgame": []}
+			teamData[team] = {"switch": [], "scale": [], "oppSwitch": [], "auto": [], "endgame": []}
+		score = getValue(match, "score_breakdown")
+
+		side, oppSide = "", ""
+		if i % 2 == 0:
+			side, oppSide = "blue", "red"
+		elif i % 2 == 1:
+			side, oppSide = "red", "blue"
+
+		fullTeamData[team]["switch"].append(score[side]["teleopSwitchOwnershipSec"])
+		fullTeamData[team]["scale"].append(score[side]["teleopScaleOwnershipSec"])
+		fullTeamData[team]["oppSwitch"].append(score[oppSide]["teleopSwitchOwnershipSec"])
+		fullTeamData[team]["auto"].append(score[side]["autoPoints"])
+		fullTeamData[team]["endgame"].append(score[side]["endgamePoints"])
+
+		#if team == 'frc811':
+			#print(getValue(match, "key"), i, side, score[side]["autoPoints"], score[side]["endgamePoints"])
+
+
+pprint.pprint(teamData)
+
+arr = []
+for i, match in enumerate(matches):
+	arr.append([])
+	arr[i].append([])
+	arr[i].append([])
+	arr[i][0].append([])
+	arr[i][0].append([])
+	arr[i][0].append([])
+	arr[i][1].append([])
+	arr[i][1].append([])
+	arr[i][1].append([])
+
+	# arr[i][n][m] (n is for alliances - 0: blue, 1: red) (m is the team # from 1-3 on that alliance)
+
+	for j in range(2):
+		for k in range(3):
+			#team = match.
+			arr[i][j][k].append(0)
+
+
+data = np.array(arr)
+#pprint.pprint(data)
+print(data.shape)
