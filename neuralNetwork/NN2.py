cfc65451e66dbfee530b1be2b8a71f7f7b78fd85
Arjun Sripathy <arjun.sripathy2000@gmail.com>
2018-03-15 19:42:44 -0700

    86.5 and save/load

--- neuralNetwork/NN2.py
+++ neuralNetwork/NN2.py
@@ -5,6 +5,11 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import math
 
+USE_LAST_NET = True
+USE_NEW = False
+USE_TWO_AGO = (not USE_LAST_NET) and (not USE_NEW)
+NUM_TEST = 2850
+GRAPH = False
 
 inputFile = open("more_data.txt")
 numData = 3350
@@ -67,8 +72,7 @@ normalizedInp = np.zeros((len(summedInp),len(summedInp[1])))
 for i in range(len(summedInp)):
 	for j in range(len(summedInp[0])):
 		normalizedInp[i][j]=(summedInp[i][j]-means[j])/stdDev[j]
-normValInp = normalizedInp[2800:]
-normalizedInp = normalizedInp[:2800]
+
 
 targetsFile = open("more_results.txt")
 dim = [numData,2]
@@ -78,8 +82,6 @@ for line in targetsFile:
 	outRaw.append(float(line.strip()))
 
 targets = constructArray(outRaw,len(dim),dim)
-targVal = targets[2800:]
-targets = targets[:2800]
 
 i = 0
 while i < len(targets):
@@ -87,6 +89,12 @@ while i < len(targets):
 		targets = np.concatenate((targets[:i],targets[(i+1):]))
 		normalizedInp = np.concatenate((normalizedInp[:i],normalizedInp[(i+1):]))
 	i+=1
+
+
+normValInp = normalizedInp[NUM_TEST:]
+normalizedInp = normalizedInp[:NUM_TEST]
+targVal = targets[NUM_TEST:]
+targets = targets[:NUM_TEST]
 
 sess = tf.InteractiveSession()
 
@@ -96,8 +104,8 @@ numHidden1 = 7
 
 
 MINI_BATCH_SIZE = 5
-EPOCHS = 300
-LEARNING_RATE = 5e-3
+EPOCHS = 30
+LEARNING_RATE = 0.8e-3
 DROPOUT = 1.0
 keep_prob = tf.placeholder(tf.float32)
 
@@ -135,21 +143,7 @@ out = tf.concat([outA,outB],1)
 wc = 0
 WPENiH = tf.reduce_mean(tf.squared_difference(WEIGHT_iH,tf.zeros(shape=tf.shape(WEIGHT_iH))))
 WPENHH = tf.reduce_mean(tf.squared_difference(WEIGHT_HH,tf.zeros(shape=tf.shape(WEIGHT_HH))))
-'''WP = tf.matmul(tf.matmul(WEIGHT_iH,WEIGHT_HH),WEIGHT_HTO)
-WPSD = tf.squared_difference(WP[:,1],WP[:,0])'''
 
-'''diffO = out[:,1]-out[:,0]
-diffT = targetValues[:,1]-targetValues[:,0]
-diffProduct = tf.multiply(diffO,diffT)
-negatives = tf.less(diffProduct,tf.ones(tf.shape(diffProduct)))
-onesnzeros = tf.where(negatives, x = tf.ones(tf.shape(negatives)),y = tf.zeros(tf.shape(negatives)))
-score0D = out[:,0]-targetValues[:,0]
-score0sqD = tf.multiply(score0D,score0D)
-score1D = out[:,1]-targetValues[:,1]
-score1sqD = tf.multiply(score1D,score1D)
-scoresqD = score0sqD+score1sqD
-mError = tf.multiply(scoresqD,onesnzeros)
-MissError = tf.reduce_mean(mError)'''
 
 targetsA = targetValues[:,:0]
 targetsB = targetValues[:,1:]
@@ -162,11 +156,10 @@ pea = tf.matmul(tf.transpose(omtA),divA)
 peb = tf.matmul(tf.transpose(omtB),divB)
 totalOutputCount = tf.cast((tf.shape(omtA)[0]*2),dtype=tf.float32)
 accuracy2 = 1-(tf.reduce_sum(pea)+tf.reduce_sum(peb))/totalOutputCount
-accuracy3 = 1-(tf.reduce_sum(pea)+tf.reduce_sum(peb))/totalOutputCount
 
 MissError = tf.reduce_mean(tf.squared_difference(out, targetValues))
 loss = MissError+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
-loss2 = tf.reduce_sum(pea)+tf.reduce_sum(peb)
+loss2 = tf.reduce_sum(pea)+tf.reduce_sum(peb)+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
 #loss = MissError-wc*tf.reduce_mean(WPSD)
 #loss = MissError
 thisLearningRate = tf.placeholder(dtype = tf.float32)
@@ -174,17 +167,17 @@ train_step = tf.train.AdamOptimizer(thisLearningRate).minimize(loss2)
 correct_prediction = tf.equal(tf.argmax(targetValues,1), tf.argmax(out,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-
 sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
 
 accuracyArr = []
 lossArr = []
 valAccArr=[]
 
-for i in range(len(targets)):
-	for j in range(len(targets[0])):
-		if(targets[i][j]==0):
-			print("afadfasdf")
+if USE_LAST_NET:
+	saver.restore(sess, "./FRCSPNetLast")
+elif USE_TWO_AGO:
+	saver.restore(sess, "./FRCSPNetTwoAgo")
 
 for i in range(1,EPOCHS+1):
 	run = True
@@ -193,58 +186,32 @@ for i in range(1,EPOCHS+1):
 		ydata = targets[j*MINI_BATCH_SIZE:(j+1)*MINI_BATCH_SIZE]
 		outLength = MINI_BATCH_SIZE
 		train_step.run(feed_dict={inputs: xdata, targetValues: ydata, thisLearningRate: LEARNING_RATE, keep_prob: DROPOUT})
-		'''l = loss.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-		print(l)
-		print("WeightProduct")
-		print(sess.run(tf.matmul(tf.matmul(WEIGHT_iH,WEIGHT_HH),WEIGHT_HTO)))
-		if j>20:
-			o = sess.run(out,feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-			print("woo hoo")
-			for k in range(50):
-				print(o[k])
-			print("WeightProduct")
-			print(sess.run(tf.matmul(tf.matmul(WEIGHT_iH,WEIGHT_HH),WEIGHT_HTO)))
-			break'''
-	'''o = sess.run(out,feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	print("WeightProduct")
-	print(sess.run(tf.matmul(tf.matmul(WEIGHT_iH,WEIGHT_HH),WEIGHT_HTO)))
-	print("Output")
-	for k in range(50):
-		print(o[k])
-	print("onesnzeros")
-	print(onesnzeros.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0}))
-	print("scoresqd")
-	print(scoresqD.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0}))
-
-	w = WPSD.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	print(w)
-	print("WeightProduct")
-	print(sess.run(tf.matmul(tf.matmul(WEIGHT_iH,WEIGHT_HH),WEIGHT_HTO)))
-	o = sess.run(out,feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	print("Output")
-	for k in range(5):
-		print(o[k])
-		print(targets[k])
-		print(" ")'''
 	a = accuracy.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
 	a2 = accuracy2.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
 	valAcc = accuracy2.eval(feed_dict={inputs: normValInp, targetValues: targVal, keep_prob: 1.0})
 	l2 = loss2.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
 	l = loss.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
+	print("Epoch ", i,"WL%: ",a," Acc (1-PE)%: ",a2,"ValidAcc: ",valAcc,"PEErr: ",l2,"SQErr: ",l)
 	if (a<0.5):
 		sess.run(tf.global_variables_initializer())
-		print("Lol we messed up so we reinitialized")
-	print("Epoch ", i,"WL%: ",a," Acc (1-PE)%: ",a2,"ValidAcc: ",valAcc,"PEErr: ",l2,"SQErr: ",l)
-	if i%10==0 and i>0:
-		if abs(a2-accuracyArr[len(accuracyArr)-10]<0.002):
+		print("Reinitialized")
+	if i%2==0 and i>10:
+		if a2-accuracyArr[len(accuracyArr)-10]<0:
 			LEARNING_RATE*=0.95
-			print(LEARNING_RATE)
-
+			print("LR dropped to ",LEARNING_RATE)
 	accuracyArr.append(a2)
 	lossArr.append(l2)
 	valAccArr.append(valAcc)
 
+print("Final Learning Rate ", LEARNING_RATE)
 
+
+saver.save(sess,"./good1")
+'''saver.save(sess, "./FRCSPNetTemp")
+saver.restore(sess, "./FRCSPNetLast")
+saver.save(sess, "./FRCSPNetTwoAgo")
+saver.restore(sess, "./FRCSPNetTemp")
+saver.save(sess, "./FRCSPNetLast")'''
 
 maxAcc = 0.0
 for i in accuracyArr:
@@ -252,13 +219,13 @@ for i in accuracyArr:
 		maxAcc=i
 print("Maximum Accuracy %g"%maxAcc)
 
+if(GRAPH):
+	xPlot = []
+	for i in range(1,EPOCHS+1):xPlot.append(i)
 
-xPlot = []
-for i in range(1,EPOCHS+1):xPlot.append(i)
-
-plt.plot(xPlot, accuracyArr, 'b-', label='Accuracy')
-plt.plot(xPlot, valAccArr, 'g-', label='ValAccuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+	plt.plot(xPlot, accuracyArr, 'b-', label='Accuracy')
+	plt.plot(xPlot, valAccArr, 'g-', label='ValAccuracy')
+	plt.xlabel('Epoch')
+	plt.ylabel('Loss')
+	plt.legend()
+	plt.show()
