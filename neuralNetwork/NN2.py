464eb2f0f7f3ee8a8efff15532c2134063170688
Arjun Sripathy <arjun.sripathy2000@gmail.com>
2018-03-22 20:51:23 -0700

    polished and documented

--- neuralNetwork/NN2.py
+++ neuralNetwork/NN2.py
@@ -1,27 +1,28 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 import math
 
+TRAIN = 0
+USE = 1
+
+MODE = USE
+'''
+	On Train network using input data from 'data.txt' and target data from 'results.txt'
+	Trains a feed-forward neuralnetwork that uses information about past matches 
+	from teams in competiting alliances to predict the match score.
+	Network predicts the correct winner 84 percent of the time.
+	Network predicts the correct score with an average percent error of 23%
+
+	On Use mode network takes input data from 'MatchInput.txt' and prints the networks 
+	predicted match score
+'''
+
+'''Uses previously saved net or creates new'''
 USE_LAST_NET = True
-USE_NEW = False
+USE_NEW = not USE_LAST_NET
 SAVE_THIS = True
 USE_TWO_AGO = (not USE_LAST_NET) and (not USE_NEW)
-NUM_TEST = 3700
-GRAPH = False
-
-inputFile = open("lots_of_data.txt")
-numData = 4193
-dim = [numData,54]
-numStats = 9
-inpRaw = []
-c = 0
-for line in inputFile:
-	inpRaw.append(float(line.strip()))
-
-
 
 def constructArray(rawInput, numDimensions, dimensionsArray):
 	if(numDimensions==1):
@@ -39,6 +40,26 @@ def weight_variable(shape):
 def bias_variable(shape):
 	initial = tf.constant(10,dtype= tf.float32,shape=shape)
 	return tf.Variable(initial)
+
+inputFile = open("data.txt")
+
+'''Total number of data samples'''
+numData = 4193
+'''Number of test data samples rest used for validation'''
+NUM_TEST = 3700
+
+dim = [numData,54]
+numStats = 9
+inpRaw = []
+c = 0
+
+'''Reads data file, stores data, normalized data'''
+
+for line in inputFile:
+	inpRaw.append(float(line.strip()))
+
+inp = constructArray(inpRaw,len(dim),dim)
+
 
 inp = constructArray(inpRaw,len(dim),dim)
 
@@ -65,17 +86,15 @@ stdDev = np.zeros(variance.shape[0])
 for i in range(len(stdDev)):
 	stdDev[i] = math.sqrt(variance[i])
 
-def sigmoidSD(x):
-	return (1/(1+math.exp(-x)))-1/2
-
-
-normalizedInp = np.zeros((len(summedInp),len(summedInp[1])))
+allNormalizedInp = np.zeros((len(summedInp),len(summedInp[1])))
 for i in range(len(summedInp)):
 	for j in range(len(summedInp[0])):
-		normalizedInp[i][j]=(summedInp[i][j]-means[j])/stdDev[j]
+		allNormalizedInp[i][j]=(summedInp[i][j]-means[j])/stdDev[j]
 
 
-targetsFile = open("lots_of_results.txt")
+'''Reads data file, stores target data, normalizes data'''
+
+targetsFile = open("results.txt")
 dim = [numData,2]
 outRaw = []
 c = 0
@@ -106,27 +125,51 @@ i = 0
 while i < len(targets):
 	if targets[i][0] == 0 or targets[i][1] == 0:
 		targets = np.concatenate((targets[:i],targets[(i+1):]))
-		normalizedInp = np.concatenate((normalizedInp[:i],normalizedInp[(i+1):]))
+		allNormalizedInp = np.concatenate((allNormalizedInp[:i],allNormalizedInp[(i+1):]))
 	i+=1
 
 
-normValInp = normalizedInp[NUM_TEST:]
-normalizedInp = normalizedInp[:NUM_TEST]
+normValInp = allNormalizedInp[NUM_TEST:]
+normalizedTrainInp = allNormalizedInp[:NUM_TEST]
 targVal = targets[NUM_TEST:]
-targets = targets[:NUM_TEST]
+targTrain = targets[:NUM_TEST]
+
+within = 50
+wInp = []
+wTarg = []
+nInp = []
+nTarg = []
+for i in range(len(targets)):
+	diff = abs(targets[i][1]-targets[i][0])
+	if(diff<within):
+		wInp.append(allNormalizedInp[i])
+		wTarg.append(targets[i])
+	else:
+		nInp.append(allNormalizedInp[i])
+		nTarg.append(targets[i])
 
 sess = tf.InteractiveSession()
 
+'''Network architecture 18 input neurons 30 first layer hidden neurons....'''
+
 insizenum=18
-numHidden = 20
-numHidden1 = 10
+numHidden = 30
+numHidden1 = 20
+numHidden2 = 10
 
-
+'''Hyperparameters'''
 MINI_BATCH_SIZE = 5
 EPOCHS = 100
-LEARNING_RATE = 5e-3
+LEARNING_RATE = 5e-5
 DROPOUT = 1.0
 keep_prob = tf.placeholder(tf.float32)
+
+
+'''
+	First 9 input neurons represent stats for the alliance whose score
+	is being determined.  Next 9 represent the opposition.  By swapping
+	the order the score of the opposing alliance is predicted
+'''
 
 inputs = tf.placeholder(dtype = tf.float32, shape = [None,insizenum])
 targetValues = tf.placeholder(dtype=tf.float32, shape = [None,2])
@@ -143,9 +186,14 @@ bias_H1 = bias_variable([numHidden1])
 hidden1 = tf.sigmoid(tf.matmul(hiddenDrop,WEIGHT_HH)+bias_H1)-0.5
 hidden1Drop = tf.nn.dropout(hidden1,keep_prob)
 
-WEIGHT_HTO = weight_variable([numHidden1,1])
+WEIGHT_HH2 = weight_variable([numHidden1,numHidden2])
+bias_H2 = bias_variable([numHidden2])
+hidden2 = tf.sigmoid(tf.matmul(hidden1Drop,WEIGHT_HH2)+bias_H2)-0.5
+hidden2Drop = tf.nn.dropout(hidden2,keep_prob)
+
+WEIGHT_HTO = weight_variable([numHidden2,1])
 bias_O = bias_variable([1])
-outA = tf.matmul(hidden1Drop,WEIGHT_HTO)+bias_O
+outA = tf.matmul(hidden2Drop,WEIGHT_HTO)+bias_O
 outA = outA*stdDevS+meanScore
 
 inputsB = tf.concat([inputs[:,9:],inputs[:,:9]],1)
@@ -156,28 +204,23 @@ hiddenBDrop = tf.nn.dropout(hiddenB,keep_prob)
 hidden1B = tf.sigmoid(tf.matmul(hiddenBDrop,WEIGHT_HH)+bias_H1)-0.5
 hidden1BDrop = tf.nn.dropout(hidden1B,keep_prob)
 
-outB = tf.matmul(hidden1BDrop,WEIGHT_HTO)+bias_O
+hidden2B = tf.sigmoid(tf.matmul(hidden1BDrop,WEIGHT_HH2)+bias_H2)-0.5
+hidden2BDrop = tf.nn.dropout(hidden2B,keep_prob)
+
+outB = tf.matmul(hidden2BDrop,WEIGHT_HTO)+bias_O
 outB = outB*stdDevS+meanScore
 
 out = tf.concat([outA,outB],1)
 
-wc = 0
-WPENiH = tf.reduce_mean(tf.squared_difference(WEIGHT_iH,tf.zeros(shape=tf.shape(WEIGHT_iH))))
-WPENHH = tf.reduce_mean(tf.squared_difference(WEIGHT_HH,tf.zeros(shape=tf.shape(WEIGHT_HH))))
+'''Weight penalty used to help prevent overfitting'''
+wc = 10
+
+WPENiH = tf.reduce_mean(tf.sqrt(tf.sqrt(tf.squared_difference(WEIGHT_iH,tf.zeros(shape=tf.shape(WEIGHT_iH))))))
+WPENHH = tf.reduce_mean(tf.sqrt(tf.sqrt(tf.squared_difference(WEIGHT_HH,tf.zeros(shape=tf.shape(WEIGHT_HH))))))
 
 
 targetsA = targetValues[:,:1]
 targetsB = targetValues[:,1:]
-
-'''omtA = abs(outA-targetsA)
-omtB = abs(outB-targetsB)
-divA = 1/targetsA
-divB = 1/targetsB
-pea = tf.matmul(tf.transpose(omtA),divA)
-peb = tf.matmul(tf.transpose(omtB),divB)
-totalOutputCount = tf.cast((tf.shape(omtA)[0]*2),dtype=tf.float32)
-accuracy2 = 1-(tf.reduce_sum(pea)+tf.reduce_sum(peb))/totalOutputCount'''
-
 
 omtA = abs(outA-targetsA)
 omtB = abs(outB-targetsB)
@@ -186,28 +229,15 @@ peb = 2*omtB/(outB+targetsB)
 totalOutputCount = tf.cast((tf.shape(omtA)[0]*2),dtype=tf.float32)
 accuracy2 = 1-(tf.reduce_sum(pea)+tf.reduce_sum(peb))/totalOutputCount
 
-'''omt2A = tf.matmul(tf.transpose(omtA),omtA)
-omt2B = tf.matmul(tf.transpose(omtB),omtB)
-chia = omt2A/targetsA
-chib = omt2B/targetsB
 
-diffOut = outB-outA
-diffTarg = targetsB-targetsA
-diffDiff = abs(diffOut-diffTarg)
-pedd = 2*diffDiff/(abs(diffOut)+abs(diffTarg))'''
-
+'''Squared error loss used at times'''
 MissError = tf.reduce_mean(tf.squared_difference(out, targetValues))
 loss = MissError+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
-#loss2 = tf.reduce_sum(chia)+tf.reduce_sum(chib)+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
-#loss2 = tf.reduce_sum(omtA)+tf.reduce_sum(omtB)
-loss2 = tf.reduce_sum(pea)+tf.reduce_sum(peb)+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
-#loss2 = tf.reduce_mean(tf.squared_difference(outB-outA, targetsB-targetsA))
-#loss2 = tf.reduce_mean(((outB-outA)-(targetsB-targetsA))))
-#loss2 = tf.reduce_sum(pedd)
-#loss2 = 1-accuracy2
-#loss2 = tf.reduce_sum(tf.matmul(tf.transpose(pea),pea))+tf.reduce_sum(tf.matmul(tf.transpose(peb),peb))+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
-#loss = MissError-wc*tf.reduce_mean(WPSD)
-#loss = MissError
+
+'''Huber loss used other times'''
+loss2 = tf.losses.huber_loss(labels=targetValues,predictions=out,delta=40)+wc*(tf.reduce_mean(WPENiH)+tf.reduce_mean(WPENHH))
+
+'''Measures correct winner predicted accuracy'''
 correct_prediction = tf.equal(tf.argmax(targetValues,1), tf.argmax(out,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 thisLearningRate = tf.placeholder(dtype = tf.float32)
@@ -221,90 +251,96 @@ accuracyArr = []
 lossArr = []
 valAccArr=[]
 
+'''Loads previous data set if neeeded'''
 if USE_LAST_NET:
 	saver.restore(sess, "./FRCSPNetLast")
 elif USE_TWO_AGO:
 	saver.restore(sess, "./FRCSPNetTwoAgo")
 
-for i in range(1,EPOCHS+1):
-	run = True
-	for j in range(int(len(inp)/MINI_BATCH_SIZE)-1):
-		xdata = normalizedInp[j*MINI_BATCH_SIZE:(j+1)*MINI_BATCH_SIZE]
-		ydata = targets[j*MINI_BATCH_SIZE:(j+1)*MINI_BATCH_SIZE]
-		outLength = MINI_BATCH_SIZE
-		train_step.run(feed_dict={inputs: xdata, targetValues: ydata, thisLearningRate: LEARNING_RATE, keep_prob: DROPOUT})
-	a = accuracy.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	a2 = accuracy2.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	valAcc = accuracy2.eval(feed_dict={inputs: normValInp, targetValues: targVal, keep_prob: 1.0})
-	l2 = loss2.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	l = loss.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-	print("Epoch ", i,"WL%: ",a," Acc (1-PE)%: ",a2,"ValidAcc: ",valAcc,"PEErr: ",l2,"SQErr: ",l)
-	if (a<0.5):
-		sess.run(tf.global_variables_initializer())
-		print("Reinitialized")
-	'''if i%2==0 and i>10:
-		if a2-accuracyArr[len(accuracyArr)-10]<0:
-			LEARNING_RATE*=0.95
-			print("LR dropped to ",LEARNING_RATE)
-	if i%2==0 and i>10:
-		if a2-accuracyArr[len(accuracyArr)-10]<0:
-			LEARNING_RATE*=0.95
-			'''
-	accuracyArr.append(a2)
-	lossArr.append(l2)
-	valAccArr.append(valAcc)
-
-print("Final Learning Rate ", LEARNING_RATE)
-
-'''
-valOutputs = sess.run(out,feed_dict={inputs: normValInp, targetValues: targVal, keep_prob: 1.0})
-
-c=0
-num=0
-outE = out.eval(feed_dict={inputs: normalizedInp, targetValues: targets, keep_prob: 1.0})
-p2 = abs(outE[:,1]-targets[:,1])
-print(p2)
-divT = 1/np.transpose(targets[:,1])
-print(divT)
-s = np.matmul(divT,p2)
-print(s)
-for i in range(len(targets)):
-	t = targets[i]
-	o = outE[i]
-	p1 = abs((t[0]-o[0])/t[0])
-	p2 = abs((t[1]-o[1])/t[1])
-	pa = (p1+p2)/2
-	if(pa>0):
-		print(t)
-		print(o)
-		print(pa)
-		print("")
-		c += (p1+p2)
-		num +=1
-print(num)
-print(c)'''
+if MODE==TRAIN :
+	'''Train network'''
+	for i in range(1,EPOCHS+1):
+		run = True
+		trainingData = normalizedTrainInp
+		trainingTargets = targTrain
+		for j in range(int(len(trainingData)/MINI_BATCH_SIZE)-1):
+			'''Train with minibatch'''
+			xdata = trainingData[j*MINI_BATCH_SIZE:(j+1)*MINI_BATCH_SIZE]
+			ydata = trainingTargets[j*MINI_BATCH_SIZE:(j+1)*MINI_BATCH_SIZE]
+			outLength = MINI_BATCH_SIZE
+			train_step.run(feed_dict={inputs: xdata, targetValues: ydata, thisLearningRate: LEARNING_RATE, keep_prob: DROPOUT})
+		a = accuracy.eval(feed_dict={inputs: trainingData, targetValues: trainingTargets, keep_prob: 1.0})
+		aV = accuracy.eval(feed_dict={inputs: normValInp, targetValues: targVal, keep_prob: 1.0})
+		a2 = accuracy2.eval(feed_dict={inputs: trainingData, targetValues: trainingTargets, keep_prob: 1.0})
+		valAcc = accuracy2.eval(feed_dict={inputs: normValInp, targetValues: targVal, keep_prob: 1.0})
+		l2 = loss2.eval(feed_dict={inputs: trainingData, targetValues: trainingTargets, keep_prob: 1.0})
+		l = loss.eval(feed_dict={inputs: trainingData, targetValues: trainingTargets, keep_prob: 1.0})
+		'''Outputs critical values each Epoch'''
+		print("Epoch ", i,"WL%: ",a,"VWL%: ",a2," Acc (1-PE)%: ",a2,"ValidAcc: ",valAcc,"Loss: ",l2,"SQErr: ",l)
+		'''Prevents network from getting stuck at bad minimum'''
+		if (a<0.4):
+			sess.run(tf.global_variables_initializer())
+			print("Reinitialized")
+		accuracyArr.append(a2)
+		lossArr.append(l2)
+		valAccArr.append(valAcc)
 
 
-if(SAVE_THIS):
-	saver.save(sess, "./FRCSPNetTemp")
-	saver.restore(sess, "./FRCSPNetLast")
-	saver.save(sess, "./FRCSPNetTwoAgo")
-	saver.restore(sess, "./FRCSPNetTemp")
-	saver.save(sess, "./FRCSPNetLast")
+	'''Confusion matrix of sorts, details how network fares in close and clear matches'''
+	print("close matches: ",len(wInp))
 
-maxAcc = 0.0
-for i in accuracyArr:
-	if(i>maxAcc):
-		maxAcc=i
-print("Maximum Accuracy %g"%maxAcc)
+	a = accuracy.eval(feed_dict={inputs: wInp, targetValues: wTarg, keep_prob: 1.0})
+	a2 = accuracy2.eval(feed_dict={inputs: wInp, targetValues: wTarg, keep_prob: 1.0})
+	l2 = loss2.eval(feed_dict={inputs: wInp, targetValues: wTarg, keep_prob: 1.0})
+	l = loss.eval(feed_dict={inputs: wInp, targetValues: wTarg, keep_prob: 1.0})
+	print("WL%: ",a," Acc (1-PE)%: ",a2,"Loss: ",l2,"SQErr: ",l)
 
-if(GRAPH):
-	xPlot = []
-	for i in range(1,EPOCHS+1):xPlot.append(i)
 
-	plt.plot(xPlot, accuracyArr, 'b-', label='Accuracy')
-	plt.plot(xPlot, valAccArr, 'g-', label='ValAccuracy')
-	plt.xlabel('Epoch')
-	plt.ylabel('Loss')
-	plt.legend()
-	plt.show()
+	print("clear wins: ",len(nInp))
+
+	a = accuracy.eval(feed_dict={inputs: nInp, targetValues: nTarg, keep_prob: 1.0})
+	a2 = accuracy2.eval(feed_dict={inputs: nInp, targetValues: nTarg, keep_prob: 1.0})
+	l2 = loss2.eval(feed_dict={inputs: nInp, targetValues: nTarg, keep_prob: 1.0})
+	l = loss.eval(feed_dict={inputs: nInp, targetValues: nTarg, keep_prob: 1.0})
+	print("WL%: ",a," Acc (1-PE)%: ",a2,"Loss: ",l2,"SQErr: ",l)
+
+	'''Save network if needed'''
+	if(SAVE_THIS):
+		saver.save(sess, "./FRCSPNetTemp")
+		saver.restore(sess, "./FRCSPNetLast")
+		saver.save(sess, "./FRCSPNetTwoAgo")
+		saver.restore(sess, "./FRCSPNetTemp")
+		saver.save(sess, "./FRCSPNetLast")
+
+	maxAcc = 0.0
+	for i in accuracyArr:
+		if(i>maxAcc):
+			maxAcc=i
+	print("Maximum Accuracy %g"%maxAcc)
+
+if MODE == USE:
+	'''Evaluate prediction'''
+	inputFile = open("MatchInput.txt")
+	dim = [1,54]
+	numStats = 9
+	inp = []
+
+	for line in inputFile:
+		inp.append(float(line.strip()))
+
+	red = []
+	blue = []
+	for j in range(numStats):
+		red.append(inp[j]+inp[j+numStats]+inp[j+numStats*2])
+		blue.append(inp[j+numStats*3]+inp[j+numStats*4]+inp[j+numStats*5])
+	summedInp = np.concatenate((red,blue))
+	normalizedInp = []
+	for i in range(len(summedInp)):
+		normalizedInp.append((summedInp[i]-means[i])/stdDev[i])
+	doubleInp = []
+	doubleInp.append(normalizedInp)
+	doubleInp.append(normalizedInp)
+	blank = [[0,0],[0,0]]
+	prediction = out.eval(feed_dict={inputs: doubleInp,keep_prob: DROPOUT})
+	print(prediction[0])
+
